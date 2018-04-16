7a1517f11e48ad5b3c50d8129221f2a373abdf4d
Anna Madej <aniamadej123@gmail.com>
2018-04-16 09:38:50 +0200

    @Configuration @ConponentScan

--- core/src/main/java/academy/learnprogramming/Main.java
+++ core/src/main/java/academy/learnprogramming/Main.java
@@ -3,18 +3,16 @@ package academy.learnprogramming;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
     private static final Logger log = LoggerFactory.getLogger(Main.class);
-
-    private static final String CONFIG_LOCATION = "beans.xml";
 
     public static void main(String[] args) {
         // log.info("guess the number game!");
 
         // create the context
-        ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(CONFIG_LOCATION);
+        ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
         NumberGenerator numberGenerator = context.getBean(NumberGenerator.class);
 
