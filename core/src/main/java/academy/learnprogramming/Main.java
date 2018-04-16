32f9eb69b274b16edfddda9c5a72fbddca5e2ef1
Anna Madej <anna.madej.krakow@gmail.com>
2018-04-16 07:39:37 +0200

    constructor and setter based DI

---
+++ core/src/main/java/academy/learnprogramming/Main.java
@@ -0,0 +1,34 @@
+package academy.learnprogramming;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
+    private static final String CONFIG_LOCATION = "beans.xml";
+
+    public static void main(String[] args) {
+        // log.info("guess the number game!");
+
+        // create the context
+        ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(CONFIG_LOCATION);
+
+        NumberGenerator numberGenerator = context.getBean("NumberGenerator", NumberGenerator.class);
+
+        int number = numberGenerator.next();
+        log.info("number = {}", number);
+
+
+        // get game bean from context (container)
+        Game game = context.getBean("Game", Game.class);
+
+        // call reset method
+        game.reset();
+
+        // close context (container)
+        context.close();
+    }
+}
