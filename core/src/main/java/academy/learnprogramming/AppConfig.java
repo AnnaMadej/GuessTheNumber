bfbfe62133a64dd34e3e9c897be8475018e5d33b
Anna Madej <aniamadej123@gmail.com>
2018-04-16 10:47:38 +0200

    added console module and @EventListener

--- core/src/main/java/academy/learnprogramming/AppConfig.java
+++ core/src/main/java/academy/learnprogramming/AppConfig.java
@@ -1,10 +1,11 @@
 package academy.learnprogramming;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-//@ComponentScan(basePackages = "academy.learnprogramming")
+@ComponentScan(basePackages = "academy.learnprogramming")
 public class AppConfig {
 
     // == bean methods
